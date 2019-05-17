0f275d7e90807a7867d82ffeeec35fd53486d0f5
Administrator <renveg2010@gmail.com>
2019-05-17 08:36:45 -0400

    changed library

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,6 @@
 void buildImage(String imageName) {
-    def sampleAppImage = docker.build(imageName) 
-    sampleAppImage.push()
+    //def sampleAppImage = docker.build(imageName) 
+    //sampleAppImage.push()
+    sh("docker build -t ${imageName} .")
+    sh("docker push ${imageName}")
 }
