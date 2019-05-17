92e4d4c7c28c74572ef1733f7b4d17363d89f3a4
Administrator <renveg2010@gmail.com>
2019-05-17 08:59:26 -0400

    added docker creds

--- vars/build.groovy
+++ vars/build.groovy
@@ -2,5 +2,10 @@ void buildImage(String imageName) {
     //def sampleAppImage = docker.build(imageName) 
     //sampleAppImage.push()
     sh("docker build -t ${imageName} .")
-    sh("docker push ${imageName}")
+    withCredentials([
+        usernamePassword(credentialsId: "docker", usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASSWORD')
+    ]){
+        sh("docker login --username=${DOCKER_USER} --password=${DOCKER_PASSWORD}")
+        sh("docker push ${imageName}")
+    }
 }
